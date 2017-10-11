749ab84d1afc218877f6da0d8191a650e68dae37
FIC <alandermm@hotmail.com>
2017-10-10 21:55:58 -0300

    criei arquivo csv

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace codexparquivocsv
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua idade: ");
+            string idade = Console.ReadLine();
+            
+
+            StreamWriter sw = new StreamWriter("Arquivo.csv", true);
+            sw.WriteLine(nome + ";" + idade);
+            sw.Close();
+        }
+    }
+}
